5fd4a182adacbe7009a3d6ea7bcc8ed26f5b6b76
GabrielH-NH <gabrielhenriquenh@gmail.com>
2023-05-15 21:06:47 -0300

    melhoria no slide

--- js/app.js
+++ js/app.js
@@ -1,2 +1,9 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+const myCarouselElement = document.querySelector('#carouselCidades')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 200,
+  touch: false
+})
